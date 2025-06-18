a956455fb31ae3a1d7b8b70189b060c3e981cf20
WhereDidMyDivGo <ahmadgameover3@gmail.com>
2025-06-18 14:08:45 +0400

    ...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,65 @@
-import { Link } from "react-router-dom";
-import logo from "../../assets/shared/desktop/logo.svg";
 import "./Header.css";
+import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+
+import logo from "../../assets/shared/desktop/logo.svg";
+import menuButton from "../../assets/shared/mobile/menu.svg";
+import closeMenuButton from "../../assets/shared/mobile/close.svg";
 
 function Header() {
+  useEffect(() => {
+    const menuBtn = document.querySelector(".menu-button");
+    const dropdownNav = document.querySelector(".dropdown-nav");
+    const dropDownAnchor = document.querySelectorAll(".anchor");
+
+    menuBtn.addEventListener("click", () => {
+      menuBtnChange();
+      toggleDropDown();
+    });
+
+    dropDownAnchor.forEach((anchor) => {
+      anchor.addEventListener("click", () => {
+        menuBtnChange();
+        toggleDropDown();
+      });
+    });
+
+    function toggleDropDown() {
+      if (dropdownNav.classList.contains("show")) {
+        dropdownNav.classList.add("fade-out");
+        setTimeout(() => {
+          dropdownNav.classList.remove("show");
+          dropdownNav.classList.remove("fade-out");
+        }, 300);
+      } else {
+        dropdownNav.classList.add("fade-in");
+        dropdownNav.classList.add("show");
+        setTimeout(() => {
+          dropdownNav.classList.remove("fade-in");
+        }, 300);
+      }
+    }
+
+    function menuBtnChange() {
+      if (menuBtn.src.includes(menuButton)) {
+        menuBtn.src = closeMenuButton;
+        menuBtn.style.width = "16px";
+        menuBtn.style.height = "15px";
+      } else {
+        menuBtn.src = menuButton;
+        menuBtn.style.width = "20px";
+        menuBtn.style.height = "6px";
+      }
+    }
+  });
+
   return (
     <>
       <header>
         <Link to="/">
           <img src={logo} />
         </Link>
-        <nav>
+        <nav className="main-nav">
           <Link to="/Stories">
             <p>STORIES</p>
           </Link>
@@ -17,13 +67,32 @@ function Header() {
             <p>FEATURES</p>
           </Link>
           <Link to="/Pricing">
-            <p>PRICING  </p>
+            <p>PRICING </p>
           </Link>
         </nav>
 
-        <button>
+        <button className="invite-button">
           <p>GET AN INVITE</p>
         </button>
+
+        <img className="menu-button" src={menuButton} />
+        <nav className="dropdown-nav">
+          <Link to="/Stories">
+            <p className="anchor">STORIES</p>
+          </Link>
+          <Link to="/Features">
+            <p className="anchor">FEATURES</p>
+          </Link>
+          <Link to="/Pricing">
+            <p className="anchor">PRICING </p>
+          </Link>
+
+          <span></span>
+
+          <button className="invite-button-2">
+            <p>GET AN INVITE</p>
+          </button>
+        </nav>
       </header>
     </>
   );
